cafbab43e7890d3a36368c940c51ccc7c37a3642
albegosu <albegosu@gmail.com>
2024-01-03 00:53:30 +0100

    Sidebar ok, hidden, hover

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'itsme';
+
+  sidebarVisible: boolean = true;
+
+  hiddenSidebar(isVisible: boolean): void {
+    this.sidebarVisible = isVisible;
+  }
 }
--- front/src/app/components/sidebar/sidebar.component.ts
+++ front/src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
 
+  @Output() sidebarChange = new EventEmitter<boolean>();
+
+  sidebarVisible: boolean = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleSidebar(): void {
+    this.sidebarVisible = !this.sidebarVisible;
+    this.sidebarChange.emit(this.sidebarVisible);
+  }
 }
