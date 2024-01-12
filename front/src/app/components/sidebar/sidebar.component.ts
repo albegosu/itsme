541dfd0f92e32930bfd85942827ac4155ce4905a
albegosu <albegosu@gmail.com>
2024-01-12 01:51:52 +0100

    Sidebar fixed, oculta al scroll

--- front/src/app/components/sidebar/sidebar.component.ts
+++ front/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -16,8 +16,30 @@ export class SidebarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event): void {
+    // Puedes ajustar el valor según cuánto quieres que el usuario haga scroll antes de ocultar el sidebar
+    const scrollOffset = 75;
+
+    if (window.pageYOffset > scrollOffset) {
+      this.hideSidebar();
+    } else {
+      this.showSidebar();
+    }
+  }
+
   toggleSidebar(): void {
     this.sidebarVisible = !this.sidebarVisible;
+    this.sidebarChange.emit(this.sidebarVisible);
+  }
+
+  private hideSidebar(): void {
+    this.sidebarVisible = false;
+    this.sidebarChange.emit(this.sidebarVisible);
+  }
+
+  private showSidebar(): void {
+    this.sidebarVisible = true;
     this.sidebarChange.emit(this.sidebarVisible);
   }
 }
